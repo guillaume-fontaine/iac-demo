dff7d282bffa50c127f2ec40cdd6eea7fe06d102
bstocker <boris.stocker@gmail.com>
2025-03-25 11:13:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-1-7-cvh6b3qb9qb14bivkpfg-4566.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-1-11-cvh6b3qb9qb14bivkpfg-4566.direct.lab-boris.fr"
   }
 }
 
